d513cae7b49001181a5c6f419a81936eb4f679dd
Florian <fdreschel@gmail.com>
2025-01-25 11:39:06 +0100

    Implementacion Juego1

--- escenas/entidades/jugador_plataformas.gd
+++ escenas/entidades/jugador_plataformas.gd
@@ -5,10 +5,12 @@ const JUMP_POWER = -1000
 const WALK_SPEED = 450
 const ACCELETARION_SMOOTHING = 20
 
+@onready var bridge = $"../Bridge"
+
 func _process(_delta):
 	velocity.y += GRAVITY
-	if Input.is_action_just_pressed("p1_up") && is_on_floor():
+	if bridge.is_pressed("up") && is_on_floor():
 		velocity.y = JUMP_POWER
-	var x_movement = Input.get_action_strength("p1_right") - Input.get_action_strength("p1_left")
+	var x_movement = bridge.get_strength("right") - bridge.get_strength("left")
 	velocity.x = x_movement * WALK_SPEED
 	move_and_slide()
--- escenas/entidades/plataforma_burbuja.gd
+++ escenas/entidades/plataforma_burbuja.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 const JUMP_POWER = -1200
-
+ 
+@onready var bridge = $"../Bridge"
+ 
 func _ready():
 	$Area2D.body_entered.connect(on_body_entered)
 	
 func on_body_entered(player: Node):
 	print("entro")
 	player.velocity.y = -1200
+	bridge.finished = true
 	queue_free()
