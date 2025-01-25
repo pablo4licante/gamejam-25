5d8c418aab77d4d26b335a902d40b912d4eda927
Máximo Martínez <maximomzts@gmail.com>
2025-01-25 14:52:11 +0100

    añadido texto de victoria a plataformas

--- escenas/niveles/escena_plataformas_1.gd
+++ escenas/niveles/escena_plataformas_1.gd
@@ -2,11 +2,14 @@ extends Node
 
 @onready var destiny_area_2d = $Destiny/Area2D
 #@onready var bridge = $"../Bridge"
+@onready var label = $Label
 
 func _ready():
 	destiny_area_2d.body_entered.connect(on_body_entered)
+	label.text = ""
 	
 func on_body_entered(_a):
 	print("HAS GANADO")
+	label.text = "¡Ganaste!"
 	#bridge.finished = true
 	
