ca50be49bc0855ab7091607ed140f32d7f8c83f6
Máximo Martínez <maximomzts@gmail.com>
2025-01-25 17:33:25 +0100

    Estilo de max

--- escenas/managers/main.gd
+++ escenas/managers/main.gd
@@ -23,10 +23,12 @@ var nivel_seleccionado = ""
 var nivel_ultimo = ""
 
 var niveles = {			# Tipo Victoria
-	"test2": [0],
-	#"test": [0],
+	#"test2": [0],
+	##"test": [0],
 	"dispara_1": [1],
-	"escena_plataformas_1": [1]
+	#"escena_plataformas_1": [1]
+	#"escena_plataformas_2": [1]
+	#"escena_plataformas_3": [1]
 }
 var nivel_tipo_victoria = 0
 
