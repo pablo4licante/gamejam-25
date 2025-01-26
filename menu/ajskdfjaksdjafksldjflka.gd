572a5aa8ded8e742614388605269e6f1500c9add
Pablo Alicante <pabloalicanteat@hotmail.com>
2025-01-26 10:55:18 +0100

    musica arreglada

--- menu/ajskdfjaksdjafksldjflka.gd
+++ menu/ajskdfjaksdjafksldjflka.gd
@@ -1,13 +1,12 @@
 extends Control
 
-var modo_ventana = DisplayServer.WINDOW_MODE_FULLSCREEN
-
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	audio_stream_player.play()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if modo_ventana != DisplayServer.window_get_mode():
-		DisplayServer.window_set_mode(modo_ventana)
+	pass
