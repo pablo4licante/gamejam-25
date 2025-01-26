ce54385f531ef04ace8f8d37399df5a6df1612a7
Florian Dreschel <49875567+network-dev@users.noreply.github.com>
2025-01-26 03:26:14 +0100

    Confetti

--- escenas/niveles/dispara_1.gd
+++ escenas/niveles/dispara_1.gd
@@ -2,17 +2,16 @@ extends Node
 
 @onready var burbujas = $Burbujas
 @onready var cursor_dispara = $Cursor_dispara
-
-@onready var label = $Label
+ 
 @onready var bridge = $Bridge
+@onready var confetti = $Confetti
 
 func _ready():
-	cursor_dispara.burbuja_died.connect(on_burbuja_died)
-	label.text = ""
+	cursor_dispara.burbuja_died.connect(on_burbuja_died) 
 		
 func on_burbuja_died():
 	if burbujas.get_child_count()-1 == 0:
 		print("HAS GANADO")
-		label.text = "¡Ganaste!"
+		confetti.emitting = true
 		cursor_dispara.queue_free() #quitar
 		bridge.finished = true
